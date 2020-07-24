5d9f3583eb4aa846f40c573dd3414e66da44b9a2
marowanrice <marowanrice@gmail.com>
2020-07-23 20:47:26 -0400

    Add files via upload

--- BookLoverErrors.py
+++ BookLoverErrors.py
@@ -1,23 +1,15 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 CS 5021 | Assignment - Module 5 Live Session Exercise: Testing Activity
 
 xxxxxx | Patrick Corbett
 fwb4cx | Frederick (Will) Blickle
-xxxxxx | Krissy North
+kn3gs  | Krissy North
 mar7dh | Rowan Rice
 
 Due: Friday, 24 July 2020 at 1:00pm EST
 
-Part 1
-This script contains the "BookLover" class, designed to met specs outline
-in the activity instructions.
-
-Part 2
-This will be tested via unit test in spearate file/s. 
-    - Minimum 2x unit tests per method, using proper descriptive names
-    - do //not// need to test the __str__ method
-  
 Part 3
 This script will be copied into a new file named "BookLoverErrors".
     - This version of the code will be modified to contain several bugs
@@ -27,16 +19,10 @@ This script will be copied into a new file named "BookLoverErrors".
         “Our test suite detected X/X errors.” #our test suite
         “The other group’s test suite detected X/X errors.” #their test suite
 
-Part 4
-Thinking about our semester project for CS 5010:
-    - Describe some test cases that we may need to write (i.e., do some TDD)
-    - No need to write code: just describe three or four tests
 
-Due-outs:
-    - 1x text file containing the answer for Parts 3 and 4. 
 """
 
-class BookLover:
+class BookLoverErrors:
     def __init__(self, name, email, favGenre, numBooks=0, bookLst= None):
         #This method creates the instance
         #Mandatory args: name, email, favGenre
@@ -55,7 +41,8 @@ class BookLover:
         if (self.numBooks != len(self.bookLst)):
            self.numBooks = len(self.bookLst)
             
-            
+        self.numBooks = numBooks + 1 ## ERROR #1
+        
     
     def __str__(self):
         #This returns a string summary of the object
@@ -73,13 +60,12 @@ class BookLover:
         #returns False if book is //not// added
         newEntry = (bookName, rating)
         
-        
         if self.hasRead(bookName):
             print("Book is already in list, cannot add again")
             return False 
         else:
-            self.numBooks += 1
-            self.bookLst.append(newEntry)
+            self.numBooks += 2 ## ERROR #5
+            #self.bookLst.append(newEntry) ## ERROR #6
             print("Added {} to list with rating {}".format(bookName, rating))
             return True
         
@@ -91,21 +77,21 @@ class BookLover:
         books = [x[0] for x in self.bookLst]
         
         if bookName in books:
-            return True
+            return False  ## ERROR #2
         else:
-            return False
+            return True   ## ERROR #2
         
     def numBooksRead(self):
         #Takes no parameters/args
         #Returns total number of books person has read
         #Uses numBooks field
-        return self.numBooks
+        return (self.numBooks + 1) ## ERROR #4
     
     def favBooks(self):
         #Takes no parameter / args
         #Returns list of titles of person's most favorite books
         #Rating must be >3 (//not// inclusive of 3)
-        favBooks = [x[0] for x in self.bookLst if x[1] > 3]
+        favBooks = [x[1] for x in self.bookLst if x[1] > 3] ## ERROR #3
         return favBooks
     
 
@@ -141,4 +127,5 @@ class BookLover:
     
     
     
+
 
--- BookLover_testsuite.py
+++ BookLover_testsuite.py
@@ -17,9 +17,9 @@ xxxxxx | Patrick Corbett
 
 fwb4cx | Frederick (Will) Blickle
 
-kn3gs | Krissy North
+kn3gs  | Krissy North
 
-xxxxxxx| Rowan Rice
+mar7dh | Rowan Rice
 
 ​
 
@@ -41,7 +41,7 @@ This will be tested via unit test in spearate file/s.
 """
 
 import unittest
-from BookLover import *
+from BookLoverErrors import *
 
 class hasReadTestCase(unittest.TestCase): # inherit from unittest.TestCase
     # Unit testing hasRead() method in BookLover.py
@@ -49,9 +49,12 @@ class hasReadTestCase(unittest.TestCase): # inherit from unittest.TestCase
     def test_is_hasRead_verifying_read_correctly(self):
         # Is hasRead() method successfully verifying read
         # books from the bookLst attribute of the BookLover object 
-
+        print("\nTest method: test_is_hasRead_verifying_read_correctly:\n")
+        
+        
         # Set up
-        Will = BookLover("Will", "fwb4cx", "SciFi", numBooks=1, bookLst=[("Dune", 5)]) # create instance
+        Will = BookLover("Will", "fwb4cx", "SciFi", numBooks=1, 
+                         bookLst=[("Dune", 5)]) # create instance
         Will.addBook("Not Dune", 4)
         Will.addBook("Holes", 3)
         Will.addBook("Dune", 2)
@@ -59,24 +62,33 @@ class hasReadTestCase(unittest.TestCase): # inherit from unittest.TestCase
         print(Will.hasRead("Holes"))
         # Test
         self.assertEqual(Will.hasRead("Holes"), True)
+        
+        print("\n")
  
     def test_is_hasRead_verifying_notRead_correctly(self):
         # Is hasRead() method successfully verifying non-read
         # books from the bookLst attribute of the BookLover object 
-
+        
+        print("\nTest method: test_is_hasRead_verifying_notRead_correctly:\n")
+        
         # Set up
-        WillAgain = BookLover("Will", "fwb4cx", "SciFi", numBooks=1, bookLst=[("Dune", 5)]) # create instance
+        WillAgain = BookLover("Will", "fwb4cx", "SciFi", numBooks=1, 
+                              bookLst=[("Dune", 5)]) # create instance
         WillAgain.addBook("Not Dune", 4)
         WillAgain.addBook("Holes", 3)
         WillAgain.addBook("Dune", 2)
         print(WillAgain.bookLst)
         # Test
         self.assertEqual(WillAgain.hasRead("Not Holes"), False)
+        
+        print("\n")
 
 class addBookTestCase(unittest.TestCase):
 
     def test_addBook_is_adding_books_correctly(self):
         #Is the addBook method correctly adding books
+        
+        print("\nTest method: test_addBook_is_adding_books_correctly:\n")
 
         #Set up
         Patrick = BookLover("Patrick","pjc8cq","Fantasy")
@@ -86,10 +98,14 @@ class addBookTestCase(unittest.TestCase):
         #Test
         listshouldbe = [("Lord of the Rings", 5),("A Game of Thrones", 5)]
         self.assertEqual(Patrick.bookLst, listshouldbe)
+        
+        print("\n")
      
     def test_addBook_behaves_right_when_book_already_in_list(self):
         #Does the addBook method appropriately fail and notify
         #user when book is already in the list
+        
+        print("\nTest method: test_addBook_behaves_right_when_book_already_in_list:\n")
 
         #Set up
         OtherPatrick = BookLover("Patrick","pjc8cq","Fantasy")
@@ -97,30 +113,84 @@ class addBookTestCase(unittest.TestCase):
 
         #Test
         self.assertEqual(OtherPatrick.addBook("The Gunslinger", 4), False)
+        
+        print("\n")
 
 class numBooksReadTestCase(unittest.TestCase):
     def test_numBooksRead_works_after_initialization(self):
         #Test that numBooksRead works correctly right after initializing
         #a BookLover
+        
+        print("\nTest method: test_numBooksRead_works_after_initialization:\n")
+        
         #Set up
-        Krissy = BookLover("Krissy", "kn3gs", "Nonfiction", numBooks = 2, bookLst = [("Sapiens",4),("Hamilton", 3)])
+        Krissy = BookLover("Krissy", "kn3gs", "Nonfiction", numBooks = 2, 
+                           bookLst = [("Sapiens",4),("Hamilton", 3)])
         numshouldbe = 2
+        print(numshouldbe)
 
         #Test
         self.assertEqual(Krissy.numBooksRead(), numshouldbe)
+        
+        print("\n")
     
     def test_numBooksRead_works_when_initial_parameters_disagree(self):
         #Test that numBooksRead is still correct if the initial
         #numBooks is not the length of bookLst.
-        AlsoKrissy = BookLover("Krissy","kn3gs","Nonfiction",numBooks = 10, bookLst = [("Sapiens", 4)])
+        
+        print("\nTest method: test_numBooksRead_works_when_initial_parameters_disagree:\n")
+        
+        #set up
+        AlsoKrissy = BookLover("Krissy","kn3gs","Nonfiction",numBooks = 10, 
+                               bookLst = [("Sapiens", 4)])
         numshouldbe = 1
+        print(numshouldbe)
 
         #Test
         self.assertEqual(AlsoKrissy.numBooksRead(), numshouldbe)
+        
+        print("\n")
+        
+    def test_numBooksRead_works_when_initial_parameters_disagree2(self):
+        #Test that numBooksRead is still correct if the initial
+        #numBooks is not the length of bookLst.
+        
+        print("\nTest method: test_numBooksRead_works_when_initial_parameters_disagree2:\n")
+        
+        #set up
+        AlsoKrissy = BookLover("Krissy","kn3gs","Nonfiction", 
+                               bookLst = [("Sapiens", 4)])
+        numshouldbe = 1
+        print(numshouldbe)
+    
+        #Test
+        self.assertEqual(AlsoKrissy.numBooksRead(), numshouldbe)
+        
+        print("\n")
+        
+    def test_numBooksRead_works_when_initial_parameters_disagree3(self):
+        #Test that numBooksRead is still correct if the initial
+        #numBooks is not the length of bookLst.
+        
+        print("\nTest method: test_numBooksRead_works_when_initial_parameters_disagree3:\n")
+        
+        #set up
+        AlsoKrissy = BookLover("Krissy","kn3gs","Nonfiction",numBooks = 2) 
+                               
+        numshouldbe = 0
+        print(numshouldbe)
+    
+        #Test
+        self.assertEqual(AlsoKrissy.numBooksRead(), numshouldbe)
+        
+        print("\n")
+
 
     def test_numBooksRead_works_after_adding_book(self):
         #Test that after adding a book, the numBooksRead method
         #still functions correctly
+        
+        print("\nTest method: test_numBooksRead_works_after_adding_book:\n")
 
         #Set up
         KrissyAgain = BookLover("Krissy", "kn3gs","Nonfiction")
@@ -129,11 +199,15 @@ class numBooksReadTestCase(unittest.TestCase):
 
         #Test
         self.assertEqual(KrissyAgain.numBooksRead(), numshouldbe)
+        
+        print("\n")
 
 class favBooksTestCase(unittest.TestCase):
     def test_favBooks_works_when_there_are_favorites(self):
         #Test whether the favBooks method works when there are
         #entries with a rating of 4 or higher
+        
+        print("\nTest method: test_favBooks_works_when_there_are_favorites:\n")
     
         Rowan = BookLover("Rowan","mar7dh","violent historical dramas")
         Rowan.addBook("Peaky Blinders", 5)
@@ -143,21 +217,27 @@ class favBooksTestCase(unittest.TestCase):
         
         #Test
         self.assertEqual(Rowan.favBooks(), fav_list_should_be)
+        
+        print("\n")
 
     def test_favBooks_works_when_there_are_no_favorites(self):
         #Test whether the favBooks method correctly returns
         #a blank list when there are no entries with a rating 
         #higher than 3
+        
+        print("\nTest method: test_favBooks_works_when_there_are_no_favorites:\n")
 
         #Set up
-        RowanAgain = BookLover("Rowan","mar7dh","violent historical dramas")
-        RowanAgain.addBook("Peaky Blinders without subtitles", 3)
-        RowanAgain.addBook("insert random title here", 2)
+        RowanAgain = BookLover("Rowan","mar7dh","Magical Fantasy")
+        RowanAgain.addBook("The Name of the Wind", 3)
+        RowanAgain.addBook("Wizard's First Rule", 3)
 
         fav_list_should_be = []
 
         #Test
-        self.assertEqual(RowanAgain.favBooks(), fav_list_should_be)    
+        self.assertEqual(RowanAgain.favBooks(), fav_list_should_be)   
+        
+        print("\n")
     
 
 if __name__ == '__main__':
